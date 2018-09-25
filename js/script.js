e0937f37f769c9cfc3288868e3827b90e6c51d93
Allison Piovani <apiovani25@gmail.com>
2018-09-25 20:23:59 -0300

    # Configuracao própria.

--- js/script.js
+++ js/script.js
@@ -2,5 +2,15 @@ let states = window.localStorage.getItem('states') || '{"data": []}';
 states = JSON.parse(data.states);
 
 function getData () {
-  ajax
+  
+
+  window.localStorage; setItem('states', JSON.stringify(sates));
+}
+
+if ('serviceWorker' in navigator) {
+  navigator.serviceWorker
+  .register('./service-worker.js')
+  .then(function (reg) {
+    
+  });
 }
